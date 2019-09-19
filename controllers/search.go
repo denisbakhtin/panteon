9cf2de36f646b1f4733543e5391c8022f3db8f71
Denis Bakhtin <denis.bakhtin@gmail.com>
2019-09-19 08:43:44 +0400

    minor fixes

--- controllers/search.go
+++ controllers/search.go
@@ -3,6 +3,7 @@ package controllers
 import (
 	"fmt"
 	"net/http"
+	"strconv"
 	"strings"
 
 	"github.com/denisbakhtin/panteon/models"
@@ -11,13 +12,22 @@ import (
 
 //SearchGet handles GET /search?search= route
 func SearchGet(c *gin.Context) {
+	limit := 48
+	page, _ := strconv.Atoi(c.Query("page"))
+	if page == 0 {
+		page = 1
+	}
 	db := models.GetDB()
 	var products []models.Product
 
-	db.Preload("Images").Where("published = true AND lower(title) LIKE lower(?)", fmt.Sprintf("%%%s%%", strings.TrimSpace(c.Query("search")))).Find(&products)
+	dbq := db.Model(&models.Product{}).Where("published = true AND lower(title) LIKE lower(?)", fmt.Sprintf("%%%s%%", strings.TrimSpace(c.Query("search"))))
+	paginator := buildPaginator(dbq, c.Request.URL.Path, c.Request.URL.RawQuery, limit, page)
+	dbq.Offset((page - 1) * limit).Limit(limit).
+		Order("recommended desc, id asc").Preload("Images").Find(&products)
 
 	h := DefaultH(c)
 	h["Title"] = fmt.Sprintf("Результаты поиска по запросу: %s", c.Query("search"))
 	h["Products"] = products
+	h["Paginator"] = paginator
 	c.HTML(http.StatusOK, "search/show", h)
 }
